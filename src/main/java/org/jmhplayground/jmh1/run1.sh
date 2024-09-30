ad025492999879ee625d68a6825fb57773634b5a
mariofusco <mario.fusco@gmail.com>
2024-09-30 18:15:04 +0200

    wip

--- src/main/java/org/jmhplayground/jmh1/run1.sh
+++ src/main/java/org/jmhplayground/jmh1/run1.sh
@@ -1,1 +1,3 @@
+java -jar /home/mario/workspace/jmh-playground/target/benchmark.jar org.jmhplayground.jmh1.DeadCodeElimination1.*
+
 java -jar /home/mario/workspace/jmh-playground/target/benchmark.jar -prof perfnorm org.jmhplayground.jmh1.DeadCodeElimination3.*
